624bcc52f4c02ef11998b9f0de8a0e76f978ea44
Jonas Scheffner <jonas.scheffner@live.de>
2018-01-03 14:51:45 +0100

    Add method override

--- index.js
+++ index.js
@@ -3,12 +3,14 @@ const router = require('./src/routes');
 const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
 const { errors } = require('celebrate');
+const methodOverride = require('method-override');
 
 mongoose.connect('mongodb://localhost/backpacker', { useMongoClient: true });
 mongoose.Promise = Promise;
 
 const app = express();
-app.use(bodyParser.json());
+app.use(methodOverride('X-HTTP-Method-Override'));
+app.use(bodyParser.json({ type: ['application/json', 'application/merge-patch+json'] }));
 app.use('/api/v0/', router);
 app.use('/uploads', express.static('uploads'));
 app.listen(3000, () => console.log('App listening on port 3000!'));
