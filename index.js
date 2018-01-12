d4135afa851852f3afc1740c1ac9c12d8d2b59b6
Jonas Scheffner <jonas.scheffner@live.de>
2018-01-12 12:29:48 +0100

    Add authentication

--- index.js
+++ index.js
@@ -5,14 +5,22 @@ const mongoose = require('mongoose');
 const { errors } = require('celebrate');
 const methodOverride = require('method-override');
 const cors = require('cors');
+const passport = require('passport');
+const GoogleTokenStrategy = require('passport-google-id-token');
+const { User } = require('./src/models');
 
 mongoose.connect('mongodb://localhost/backpacker', { useMongoClient: true });
 mongoose.Promise = Promise;
+
+passport.use(new GoogleTokenStrategy({
+  clientID: '281227624759-l6dd68h5j2jlcn8scgh4g0kcn42f107l.apps.googleusercontent.com',
+}, (token, id, done) => User.find({ googleId: id }, done)));
 
 const app = express();
 app.use(cors());
 app.use(methodOverride('X-HTTP-Method-Override'));
 app.use(bodyParser.json({ type: ['application/json', 'application/merge-patch+json'] }));
+app.use(passport.authenticate('google-id-token'));
 app.use('/api/v0/', router);
 app.use('/uploads', express.static('uploads'));
 app.listen(3000, () => console.log('App listening on port 3000!'));
